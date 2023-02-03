c714af010bdca0941962b5e00e0a0978215f6945
alexanderbater <bater.a@etoncollege.org.uk>
2023-02-03 13:00:21 +0000

    EW

---
+++ Absence Recorder/View/RootTabView.swift
@@ -0,0 +1,33 @@
+//
+//  RootTabView.swift
+//  Absence Recorder
+//
+//  Created by Alexander Bater on 03/02/2023.
+//
+
+import SwiftUI
+
+struct RootTabView: View {
+    var body: some View {
+        TabView{
+            DivisionsView()
+                .tabItem{
+                    Image(systemName: "square.and.pencil")
+                    Text("Absences")
+                    
+                }
+            StatisticsView()
+                .tabItem{
+                    Image(systemName: "chart.bar")
+                    Text("Statistics")
+                    
+                }
+        }
+    }
+}
+
+struct RootTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootTabView()
+    }
+}
